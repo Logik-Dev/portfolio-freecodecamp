81040e456b53956709396ba945b172dab07e804a
Logik-Dev <logikdevfr@gmail.com>
2019-04-18 19:30:31 +0200

    css animations

---
+++ effect.js
@@ -0,0 +1,11 @@
+
+  setTimeout(() => {
+    var typed2 = new Typed('#description', {
+        strings: ['Développement Web', 'Fullstack Javascript'],
+        typeSpeed: 80,
+        backSpeed: 1500,
+        fadeOut: true,
+        loop: false,
+        cursorChar: "",
+      });
+  }, 2000)
